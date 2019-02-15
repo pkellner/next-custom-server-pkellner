b1c006d3f044872f46056ffb85b1b05339a72e38
pkellner <peter@peterkellner.net>
2019-02-15 07:51:16 -0800

    added example of websstorm redundant init problem

---
+++ pages/login.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Login(props) {
+  let z = 100;
+  let x = 11111;
+  if (z) {
+    x = 17;
+  } else {
+    x = 18;
+  }
+  console.log(x);
+
+  return (
+    <form action="/login" method="POST">
+      <br />
+      username: <input type="text" name="username" />
+      <br />
+      password: <input type="password" name="password" />
+      <br />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Login;
